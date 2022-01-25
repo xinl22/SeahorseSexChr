d2d11db418b712bb75412584eaaa8c825eb1884b
xinl22 <34504883+xinl22@users.noreply.github.com>
2022-01-25 11:14:35 +0800

    Add files via upload

---
+++ bwa.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#PBS -N bwa
+
+#PBS -o run.log
+
+#PBS -e err.log
+
+#PBS -j oe
+
+#PBS -l nodes=1:ppn=8
+
+#PBS -l walltime=9999:00:00
+
+#PBS -q batch
+
+#PBS -V
+
+#PBS -S /bin/bash
+
+genome0=$1
+read1=$2
+read2=$3
+sample=$4
+cpu=8
+
+
+bwa mem -t $cpu $genome0 $read1 $read2  |  samtools sort -@ $cpu  -O BAM -o $genome.$sample.sorted.bam  -
+
+samtools index $genome.$sample.sorted.bam
+
+
+
+
